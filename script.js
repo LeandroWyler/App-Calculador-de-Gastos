89e37dfc7075c18b4864db016d09dc19d2fdeca8
LeandroWyler <leandrowyller15@gmail.com>
2025-06-10 18:19:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-// script.js
 function login() {
   const email = document.getElementById('email').value;
   const senha = document.getElementById('senha').value;
 
   if (email === "admin@teste.com" && senha === "123") {
     alert("Login bem-sucedido!");
-    // depois redirecionamos para a tela principal
+    window.location.href = "/dashboard.html"; // redireciona corretamente
   } else {
     alert("E-mail ou senha inválidos");
   }
